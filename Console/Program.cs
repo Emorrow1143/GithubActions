19c576e75f2d4cdecba95e73d339abb1166910d1
Emorrow1143 <97996624+Emorrow1143@users.noreply.github.com>
2023-04-12 20:01:28 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace GithubActionsLab
 {
@@ -84,10 +84,11 @@ namespace GithubActionsLab
         // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            return Math.Pow(double.Parse(x), double.Parse(y));
         }
     }
 
 
 
 }
+
